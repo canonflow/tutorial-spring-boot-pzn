3532906fde68a077dfae178cdaa54a14a2c7d9b7
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2026-03-14 21:25:53 +0700

    feat(dasar): create a custom scope

--- belajar-spring-dasar/src/main/java/cloud/canonflow/spring/core/belajarspringdasar/ScopeConfiguration.java
+++ belajar-spring-dasar/src/main/java/cloud/canonflow/spring/core/belajarspringdasar/ScopeConfiguration.java
@@ -1,9 +1,13 @@
 package cloud.canonflow.spring.core.belajarspringdasar;
 
+import cloud.canonflow.spring.core.belajarspringdasar.data.Bar;
 import cloud.canonflow.spring.core.belajarspringdasar.data.Foo;
+import cloud.canonflow.spring.core.belajarspringdasar.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Scope;
 
 @Slf4j
@@ -14,5 +18,20 @@ public class ScopeConfiguration {
     public Foo foo() {
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public static CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
--- belajar-spring-dasar/src/test/java/cloud/canonflow/spring/core/belajarspringdasar/ScopeTest.java
+++ belajar-spring-dasar/src/test/java/cloud/canonflow/spring/core/belajarspringdasar/ScopeTest.java
@@ -1,5 +1,6 @@
 package cloud.canonflow.spring.core.belajarspringdasar;
 
+import cloud.canonflow.spring.core.belajarspringdasar.data.Bar;
 import cloud.canonflow.spring.core.belajarspringdasar.data.Foo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ScoreTest {
+public class ScopeTest {
     private ApplicationContext context;
 
     @BeforeEach
@@ -24,5 +25,19 @@ public class ScoreTest {
         Assertions.assertNotSame(foo1, foo2);
         Assertions.assertNotSame(foo2, foo3);
         Assertions.assertNotSame(foo3, foo1);
+    }
+
+    @Test
+    void testDoubletonScore() {
+        Bar bar1 = context.getBean(Bar.class);
+        Bar bar2 = context.getBean(Bar.class);
+        Bar bar3 = context.getBean(Bar.class);
+        Bar bar4 = context.getBean(Bar.class);
+
+        Assertions.assertSame(bar1, bar3);
+        Assertions.assertSame(bar2, bar4);
+
+        Assertions.assertNotSame(bar1, bar2);
+        Assertions.assertNotSame(bar3, bar4);
     }
 }
