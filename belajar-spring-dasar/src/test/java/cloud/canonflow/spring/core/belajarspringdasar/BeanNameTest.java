fee501f8c0fc464c34867e32cdeb87b2136b02fd
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2026-03-14 20:20:23 +0700

    feat(dasar): change bean name

---
+++ belajar-spring-dasar/src/test/java/cloud/canonflow/spring/core/belajarspringdasar/BeanNameTest.java
@@ -0,0 +1,27 @@
+package cloud.canonflow.spring.core.belajarspringdasar;
+
+import cloud.canonflow.spring.core.belajarspringdasar.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanNameTest {
+    private ApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        context = new AnnotationConfigApplicationContext(BeanNameConfiguration.class);
+    }
+
+    @Test
+    void testBeanName() {
+        Foo foo = context.getBean(Foo.class);
+        Foo fooFirst = context.getBean("fooFirst", Foo.class);
+        Foo fooSecond = context.getBean("fooSecond", Foo.class);
+
+        Assertions.assertSame(foo, fooFirst);
+        Assertions.assertNotSame(foo, fooSecond);
+    }
+}
