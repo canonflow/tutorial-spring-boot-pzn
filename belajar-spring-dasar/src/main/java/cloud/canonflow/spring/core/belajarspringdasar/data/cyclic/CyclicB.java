bd410a740a7a5f88c1d3c6575f694d215067595a
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2026-03-14 20:45:32 +0700

    feat(dasar): circular dependency

---
+++ belajar-spring-dasar/src/main/java/cloud/canonflow/spring/core/belajarspringdasar/data/cyclic/CyclicB.java
@@ -0,0 +1,10 @@
+package cloud.canonflow.spring.core.belajarspringdasar.data.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CyclicB {
+    private CyclicC cyclicC;
+}
---
+++ belajar-spring-dasar/src/main/java/cloud/canonflow/spring/core/belajarspringdasar/data/cyclic/CyclicC.java
@@ -0,0 +1,10 @@
+package cloud.canonflow.spring.core.belajarspringdasar.data.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CyclicC {
+    private CyclicA cyclicA;
+}
